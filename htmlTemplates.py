f235dca1256978da141a8b9d32dec48ed84b9f16
Reu3406 <157335953+Reu3406@users.noreply.github.com>
2024-02-23 11:23:51 +0800

    Update htmlTemplates.py
    sansitising to non-gary version

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -30,8 +30,8 @@ css = """
 bot_template = """
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://corp.nhg.com.sg/HSOR/PublishingImages/gary.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
-        <!--img src="https://www.seekpng.com/png/detail/118-1188982_doctor-clipart-clip-art-doctor-png.png" alt="Doctor Clipart - Clip Art Doctor Png@seekpng.com"-->
+        <!--img src="https://corp.nhg.com.sg/HSOR/PublishingImages/gary.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;"-->
+        <img src="https://www.seekpng.com/png/detail/118-1188982_doctor-clipart-clip-art-doctor-png.png" alt="Doctor Clipart - Clip Art Doctor Png@seekpng.com">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
